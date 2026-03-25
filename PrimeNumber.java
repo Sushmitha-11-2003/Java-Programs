3c132819c36bbc99b81d104117a36e93ebe9ea72
Sushmitha <sushmithachinnagowni03@gmail.com>
2026-03-25 11:21:37 +0530

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,18 @@
+class PrimeNumber {
+    public static void main(String[] args) {
+        int n = 7;
+        boolean isPrime = true;
+
+        for(int i = 2; i < n; i++) {
+            if(n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime)
+            System.out.println("Prime");
+        else
+            System.out.println("Not Prime");
+    }
+}
